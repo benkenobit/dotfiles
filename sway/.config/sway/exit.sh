610aec211e9e9dbe5c5e3a6ebede2c90f0a06964
Benjamin Robache <dev.benjamin.robache@gmail.com>
2024-06-07 23:44:00 +0200

    First steps to switch to tokyo night

--- sway/.config/sway/exit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ ! $(pgrep -x "swaynag") ]]; then
-    swaynag --background 16161d --border 9aa5ce --border-bottom 9aa5ce --button-background 9aa5ce \
-	    --button-border-size 0 --button-padding 8 --text 16161d --button-text 16161d --edge bottom \
-	    -t warning -m 'Do you really want to log out?' -B 'You heard me!' 'swaymsg exit'
-fi
-
